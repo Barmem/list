493fded3687433b46d459da313e76d4fad81c599
Barmem <igor.neim7@gmail.com>
2021-02-07 17:37:39 +0300

    nothing yet

--- linked_list.c
+++ linked_list.c
@@ -17,42 +17,54 @@ struct List {
 
 List *NewList() {
     //TODO: напиши меня!
+    
 }
 
 void DestroyList(List* this) {
     //TODO: напиши меня!
+
 }
 
 void Append(List *this, int value) {
     //TODO: напиши меня!
+
 }
 void Prepend(List *this, int value) {
     //TODO: напиши меня!
+
 }
 void AppendAll(List *this, const List *that) {
     //TODO: напиши меня!
+
 }
 void InsertAt(List *this, int index, int value) {
     //TODO: напиши меня!
+
 }
 
 void RemoveAt(List *this, int index) {
     //TODO: напиши меня!
+
 }
 void RemoveAll(List *this) {
     //TODO: напиши меня!
+
 }
 
 int Pop(List *this) {
     //TODO: напиши меня!
+
 }
 int Dequeue(List *this) {
     //TODO: напиши меня!
+
 }
 
 int Length(const List *this) {
     //TODO: напиши меня!
+
 }
 int GetAt(const List *this, int index) {
     //TODO: напиши меня!
+    
 }
